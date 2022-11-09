821c309a5ebe550f70b53065e277a2e299be1055
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-11-10 05:18:24 +0600

    total reviews added

--- src/pages/Home/ServicesHome.js
+++ src/pages/Home/ServicesHome.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ServicesHome = () => {
   const [services, setServices] = useState([]);
@@ -12,44 +13,48 @@ const ServicesHome = () => {
 
   return (
     <div>
-      <div className="flex mx-auto">
-        <h1 className="md:text-6xl mx-auto text-4xl font-bold title">
-          SERVICES
-        </h1>
-      </div>
-      <div className="grid justify-center m-auto justify-items-center gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 container mx-auto ">
-        {services.map((service) => (
-          <div
-            key={service._id}
-            className="card w-auto mx-2 bg-base-100 shadow-xl border"
-          >
-            <figure>
-              <img src={service.img} alt="" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{service.title}</h2>
-              <p>{service.description.slice(0, 100)}...</p>
-              <div className="card-actions flex items-center md:space-x-10">
-                <p className="mt-8 text-xl">Price: ${service.price}</p>
-                <Link
-                  to={`/services/${service._id}`}
-                  className="btn btn-outline mx-auto mt-8"
-                >
-                  VIEW DETAILS
-                </Link>
+      <PhotoProvider>
+        <div className="flex mx-auto">
+          <h1 className="md:text-6xl mx-auto text-4xl font-bold title">
+            SERVICES
+          </h1>
+        </div>
+        <div className="grid justify-center m-auto justify-items-center gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 container mx-auto ">
+          {services.map((service) => (
+            <div
+              key={service._id}
+              className="card w-auto mx-2 bg-base-100 shadow-xl border"
+            >
+              <figure>
+                <PhotoView src={service.img}>
+                  <img src={service.img} alt="" />
+                </PhotoView>
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{service.title}</h2>
+                <p>{service.description.slice(0, 100)}...</p>
+                <div className="card-actions flex items-center md:space-x-10">
+                  <p className="mt-8 text-xl">Price: ${service.price}</p>
+                  <Link
+                    to={`/services/${service._id}`}
+                    className="btn btn-outline mx-auto mt-8"
+                  >
+                    VIEW DETAILS
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex">
-        <Link
-          to="/services"
-          className="btn btn-secondary btn-wide font-bold  text-lg mx-auto mt-12"
-        >
-          VIEW ALL
-        </Link>
-      </div>
+          ))}
+        </div>
+        <div className="flex">
+          <Link
+            to="/services"
+            className="btn btn-secondary btn-wide font-bold  text-lg mx-auto mt-12"
+          >
+            VIEW ALL
+          </Link>
+        </div>
+      </PhotoProvider>
     </div>
   );
 };
--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,21 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
 
 const ServiceDetails = () => {
     const {_id, title, img, price, description } = useLoaderData();
     const { user } = useContext(AuthContext);
-    const serviceId = _id;
+    
+    const [reviews, setReviews] = useState([]);
     //console.log(serviceId);
 
+    //review add handler
   const AddReview = (event) => {
     event.preventDefault();
+    const serviceId = _id;
+    const userImg = user.photoURL;
     const name = event.target.name.value;
     const email = event.target.email.value;
     const review = event.target.review.value;
-    const reviewData = {serviceId, name, email, review};
+    const reviewData = {serviceId,userImg, name, email, review};
     
-    fetch("http://localhost:5000/reviews", {
+    fetch("http://localhost:5000/add-review", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -35,6 +39,18 @@ const ServiceDetails = () => {
       });
   };
 
+    useEffect(() => {
+      fetch("http://localhost:5000/reviews")
+        .then((res) => res.json())
+        .then((data) => {
+          const showReview = data.filter(
+            (shw) => shw.serviceId === _id
+          );
+
+          setReviews(showReview);
+        });
+    }, [_id]);
+    //console.log(reviews);
   return (
     <div>
       <section className="hero min-h-screen bg-base-200">
@@ -48,49 +64,77 @@ const ServiceDetails = () => {
         </div>
       </section>
       <section className="md:w-1/2 mx-auto">
-        <form onSubmit={AddReview}>
-          <div className="w-full p-8 my-4 md:px-12  mx-auto rounded-2xl shadow-2xl">
-            <div className="flex">
-              <h1 className="font-bold uppercase text-3xl">Leave a Review</h1>
-            </div>
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-              <input
-                name="name"
-                type="text"
-                placeholder="Name*"
-                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                required
-              />
+          <h1 className="text-3xl my-6 font-semibold">
+            Total Reviews : {reviews.length}
+          </h1>
+          {reviews.map((review) => (
+            <tbody key={review._id}>
+              <tr>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="rounded-lg w-12 h-12 my-2">
+                        <img
+                          src={review.userImg}
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{review.name}</div>
+                      <h4 className="text-sm opacity-50">{review.email}</h4>
+                    </div>
+                  </div>
+                </td>
+                <td className="mx-4">{review.review}</td>
+              </tr>
+            </tbody>
+          ))}
+        </section>
+        <div>
+          <form onSubmit={AddReview}>
+            <div className="w-full p-8 my-4 md:px-12  mx-auto rounded-2xl shadow-2xl">
+              <div className="flex">
+                <h1 className="font-bold uppercase text-3xl">Leave a Review</h1>
+              </div>
+              <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Name*"
+                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  required
+                />
 
-              <input
-                defaultValue={user?.email}
-                name="email"
-                type="email"
-                readOnly
-                placeholder="Email*"
-                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                required
-              />
+                <input
+                  defaultValue={user?.email}
+                  name="email"
+                  type="email"
+                  readOnly
+                  placeholder="Email*"
+                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  required
+                />
+              </div>
+              <div className="my-4">
+                <textarea
+                  name="review"
+                  placeholder="Review*"
+                  className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  required
+                ></textarea>
+              </div>
+              <div className="my-2 w-1/2 lg:w-1/4">
+                <button
+                  type="submit"
+                  className="btn btn-secondary uppercase text-sm font-bold tracking-wide p-3 rounded-lg w-full"
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-            <div className="my-4">
-              <textarea
-                name="review"
-                placeholder="Review*"
-                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                required
-              ></textarea>
-            </div>
-            <div className="my-2 w-1/2 lg:w-1/4">
-              <button
-                type="submit"
-                className="btn btn-secondary uppercase text-sm font-bold tracking-wide p-3 rounded-lg w-full"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </section>
+          </form>
+        </div>
     </div>
   );
 };
